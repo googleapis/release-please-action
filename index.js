11e54bb8224eb372ea16f3f94f622c67255058f9
bcoe <bencoe@google.com>
2020-05-09 15:34:05 -0700

    feat: handle creating releases

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const { GitHubRelease } = require('release-please/build/src/github-release')
 const { ReleasePRFactory } = require('release-please/build/src/release-pr-factory')
 
 const RELEASE_LABEL = 'autorelease: pending'
@@ -8,8 +9,19 @@ const action = core.getInput('action')
 async function main () {
   const token = core.getInput('token')
   const releaseType = core.getInput('release-type')
-  const name = core.getInput('name')
+  const packageName = core.getInput('package-name')
 
+  // First we check for any merged release PRs (PRs merged with the label
+  // "autorelease: pending"):
+  const gr = new GitHubRelease({
+    label: RELEASE_LABEL,
+    repoUrl: process.env.GITHUB_REPOSITORY,
+    packageName
+  })
+  await gr.createRelease();
+
+  // Next we check for PRs merged since the last release, and groom the
+  // release PR:
   const release = ReleasePRFactory.build(releaseType, {
     packageName: name || 'unknown',
     apiUrl: 'https://api.github.com',
